49fa125e900b8d4835ee62a3c8076becb431761c
Evgenii Timoshchuk <zheny.tim@gmail.com>
2021-08-17 19:20:24 +0300

    refactor scanner --> buffered reader

--- src/main/java/UserFileReader.java
+++ src/main/java/UserFileReader.java
@@ -1,61 +1,52 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class UserFileReader {
 
     public static List<User> readUsersList(String fileName) {
 
         List<User> users = new ArrayList<>();
-        Scanner scanner = null;
 
-        try {
+        int lineIdx = 0;
+        String surname = "";
+        String name = "";
+        String middleName = "";
+        Integer age = null;
+        String passport = "";
 
-            scanner = new Scanner(new File(fileName));
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName), StandardCharsets.UTF_8)) {
+            for (String line = null; (line = br.readLine()) != null;) {
+                    switch (lineIdx) {
+                        case 0:
+                            surname = line;
+                            break;
+                        case 1:
+                            name = line;
+                            break;
+                        case 2:
+                            middleName = line;
+                            break;
+                        case 3:
+                            age = Integer.parseInt(line);  // TODO: exception
+                            break;
+                        case 4:
+                            passport = line;
+                            break;
+                    }
+                    lineIdx++;
 
-            int lineIdx = 0;
-            String surname = "";
-            String name = "";
-            String middleName = "";
-            Integer age = null;
-            String passport = "";
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-
-                switch (lineIdx) {
-                    case 0:
-                        surname = line;
-                        break;
-                    case 1:
-                        name = line;
-                        break;
-                    case 2:
-                        middleName = line;
-                        break;
-                    case 3:
-                        age = Integer.parseInt(line);  // TODO: exception
-                        break;
-                    case 4:
-                        passport = line;
-                        break;
+                    if (lineIdx == 5) {
+                        lineIdx = 0;
+                        users.add(new User(surname, name, middleName, age, passport));
+                    }
                 }
-
-                lineIdx++;
-
-                if (lineIdx == 5) {
-                    lineIdx = 0;
-                    users.add(new User(surname, name, middleName, age, passport));
-                }
-
-
-            }
-        } catch (final FileNotFoundException e) {
-            System.out.println("Файл с таким именем не найден: " + e);
-        } finally {
-            scanner.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении из файла: " + e);
         }
 
         return users;
